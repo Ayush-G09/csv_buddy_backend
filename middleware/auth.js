b8531988f12b219352c55dc7f2372af4e546fc7c
Ayush-G09 <ayushgokhle@gmail.com>
2024-07-22 06:30:21 +0530

    'fix'

--- middleware/auth.js
+++ middleware/auth.js
@@ -4,7 +4,7 @@ const dotenv = require("dotenv");
 dotenv.config();
 
 module.exports = function (req, res, next) {
-  const token = req.header("x-auth-token");s
+  const token = req.header("x-auth-token");
 
   if (!token) {
     return res.status(401).json({ msg: "No token, authorization denied" });
